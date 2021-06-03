cde39123278e205777fe5520503d02cbbda45a48
Priyansh Khandelwal <52736997+priyanshk20@users.noreply.github.com>
2021-06-04 01:44:42 +0530

    till user input list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const emojiDictionary = {
+  "😀": "Happy",
+  "😉": "Wink",
+  "😣": "Irritated",
+  "🥱": "Yawning",
+  "😭": "Crying"
+};
+
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputhandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "Sorry, This is not our Database.";
+    }
+
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
       <h1>WhatMoji</h1>
-      <input></input>
+      <input onChange={emojiInputhandler}></input>
+      <h2>{meaning}</h2>
     </div>
   );
 }
