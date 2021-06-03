0f12b86a633bcc80b789cfd4516d8209bc26054f
Priyansh Khandelwal <52736997+priyanshk20@users.noreply.github.com>
2021-06-03 18:24:26 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  var [likeCounter, setLikecounter] = useState(0);
-
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Hello React</h2>
-      <button onClick={() => setLikecounter(likeCounter + 1)}>
-        Like Me
-      </button>{" "}
-      {likeCounter}
+      <h1>WhatMoji</h1>
     </div>
   );
 }
