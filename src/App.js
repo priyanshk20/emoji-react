233b843ce62088efa93d2c5a632185d6b4117ad4
Priyansh Khandelwal <52736997+priyanshk20@users.noreply.github.com>
2021-06-04 14:23:14 +0530

    app v1 complete

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ const emojiDictionary = {
   "😭": "Crying"
 };
 
+var emojisIndb = Object.keys(emojiDictionary);
+
 export default function App() {
   const [meaning, setMeaning] = useState("");
+  function emojiClickhandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
 
   function emojiInputhandler(event) {
     var userInput = event.target.value;
@@ -19,14 +25,28 @@ export default function App() {
     if (meaning === undefined) {
       meaning = "Sorry, This is not our Database.";
     }
-
     setMeaning(meaning);
   }
   return (
     <div className="App">
       <h1>WhatMoji</h1>
-      <input onChange={emojiInputhandler}></input>
+      <input
+        placeholder={"Copy any Emoji from below"}
+        onChange={emojiInputhandler}
+      ></input>
       <h2>{meaning}</h2>
+      <h3>Emojis in our Database: </h3>
+      {emojisIndb.map((emoji) => {
+        return (
+          <span
+            key={emoji}
+            style={{ padding: "1rem", fontSize: "3rem", cursor: "pointer" }}
+            onClick={() => emojiClickhandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
